1e58682fcba777344ce8c0001096e21247d64007
James Sevedge <j.sevedge@f5.com>
2018-02-06 12:27:33 -0800

    update tag versions

--- build/master_template.py
+++ build/master_template.py
@@ -41,8 +41,8 @@ route_add_cmd = ""
 ## Static Variable Assignment ##
 content_version = '4.3.0.0'
 f5_networks_tag = 'v4.3.0.0'
-f5_cloud_libs_tag = 'release-3.6.0'
-f5_cloud_libs_azure_tag = 'release-1.5.0'
+f5_cloud_libs_tag = 'v3.6.0'
+f5_cloud_libs_azure_tag = 'v1.5.0'
 f5_cloud_iapps_tag = 'v1.2.1'
 f5_cloud_workers_tag = 'v1.0.0'
 # Set BIG-IP versions to allow
